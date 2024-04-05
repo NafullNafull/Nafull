5a227ed106ae43831b152e360d07cbe4f297253d
raul <product.raul@gmail.com>
2024-04-06 07:28:31 +0900

    chore: lombok
    chore: lombok

--- build.gradle.kts
+++ build.gradle.kts
@@ -22,10 +22,12 @@ repositories {
 }
 
 dependencies {
+    compileOnly("org.projectlombok:lombok")
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.boot:spring-boot-starter-security")
     implementation("org.springframework.boot:spring-boot-starter-web")
     runtimeOnly("com.mysql:mysql-connector-j")
+    annotationProcessor("org.projectlombok:lombok")
     annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.springframework.security:spring-security-test")
