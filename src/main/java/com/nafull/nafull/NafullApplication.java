b580bbdfe691b8549f1e1dd14397326d8c23269e
raul <product.raul@gmail.com>
2024-04-06 03:30:39 +0900

    Initial Commit

---
+++ src/main/java/com/nafull/nafull/NafullApplication.java
@@ -0,0 +1,13 @@
+package com.nafull.nafull;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NafullApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NafullApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/nafull/nafull/NafullApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nafull.nafull;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NafullApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
