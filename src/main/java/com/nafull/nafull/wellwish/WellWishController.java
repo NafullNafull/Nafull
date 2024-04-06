49cf80bdc42ea1dd3c62c4acd8e9afb45edf2e58
raul <product.raul@gmail.com>
2024-04-06 09:17:20 +0900

    feat: WellWishAPI spec

--- src/main/java/com/nafull/nafull/wellwish/WellWishController.java
+++ src/main/java/com/nafull/nafull/wellwish/WellWishController.java
@@ -6,8 +6,6 @@ import com.nafull.nafull.wellwish.data.SendWellWish;
 import com.nafull.nafull.wellwish.data.WellWish;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
-
 @RestController
 @RequestMapping("/api/v1/well-wishes")
 public class WellWishController {
