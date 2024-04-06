06981e17179189af315e6fcb83f8e7cd4058f79c
raul <product.raul@gmail.com>
2024-04-06 13:18:33 +0900

    chore: jda

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,5 @@
+import java.net.URI
+
 plugins {
     java
     id("org.springframework.boot") version "3.2.4"
@@ -19,6 +21,10 @@ configurations {
 
 repositories {
     mavenCentral()
+    maven {
+        name = "m2-dv8tion"
+        url  = URI("https://m2.dv8tion.net/releases")
+    }
 }
 
 dependencies {
@@ -27,6 +33,7 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-security")
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.0.2")
+    implementation("net.dv8tion:JDA:4.4.0_350")
     runtimeOnly("com.mysql:mysql-connector-j")
     annotationProcessor("org.projectlombok:lombok")
     annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
